abca4e06c054604acd5fc7018054f8371451038f
hippalessandro <hippalessandro@U11WKS03>
2017-06-29 15:41:01 +0200

    gui erstellt

---
+++ WindowsFormsApplicationProjectX/Connection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace WindowsFormsApplicationProjectX
+{
+    class Connection
+    {
+        OleDbConnection con;
+        public Connection()
+        {
+            connect();
+        }
+
+        public void connect()
+        {
+            con = new OleDbConnection();
+            con.ConnectionString = Properties.Settings.Default.DbPath;
+            con.Open();
+        }
+    }
+}
--- WindowsFormsApplicationProjectX/FormNewTable.Designer.cs
+++ WindowsFormsApplicationProjectX/FormNewTable.Designer.cs
@@ -28,7 +28,228 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormNewTable));
+            this.pictureBoxBack = new System.Windows.Forms.PictureBox();
+            this.textBoxTabellenname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxSpalte1 = new System.Windows.Forms.TextBox();
+            this.textBoxSpalte5 = new System.Windows.Forms.TextBox();
+            this.textBoxSpalte4 = new System.Windows.Forms.TextBox();
+            this.textBoxSpalte3 = new System.Windows.Forms.TextBox();
+            this.textBoxSpalte2 = new System.Windows.Forms.TextBox();
+            this.buttonTabelleErstellen = new System.Windows.Forms.Button();
+            this.pictureBoxTabellenname = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSpalte1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSpalte2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSpalte3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSpalte5 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSpalte4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTabellenname)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSpalte1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSpalte2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSpalte3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSpalte5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSpalte4)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBoxBack
+            // 
+            this.pictureBoxBack.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxBack.Image")));
+            this.pictureBoxBack.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxBack.Name = "pictureBoxBack";
+            this.pictureBoxBack.Size = new System.Drawing.Size(48, 34);
+            this.pictureBoxBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxBack.TabIndex = 0;
+            this.pictureBoxBack.TabStop = false;
+            this.pictureBoxBack.Click += new System.EventHandler(this.pictureBoxBack_Click);
+            // 
+            // textBoxTabellenname
+            // 
+            this.textBoxTabellenname.Location = new System.Drawing.Point(257, 96);
+            this.textBoxTabellenname.Name = "textBoxTabellenname";
+            this.textBoxTabellenname.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTabellenname.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tabellenname:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Spalte 1:";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Spalte 5:";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(115, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Spalte 4:";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(115, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Spalte 3:";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(115, 164);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Spalte 2:";
+            this.label6.Visible = false;
+            // 
+            // textBoxSpalte1
+            // 
+            this.textBoxSpalte1.Location = new System.Drawing.Point(257, 130);
+            this.textBoxSpalte1.Name = "textBoxSpalte1";
+            this.textBoxSpalte1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSpalte1.TabIndex = 8;
+            this.textBoxSpalte1.Visible = false;
+            // 
+            // textBoxSpalte5
+            // 
+            this.textBoxSpalte5.Location = new System.Drawing.Point(257, 242);
+            this.textBoxSpalte5.Name = "textBoxSpalte5";
+            this.textBoxSpalte5.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSpalte5.TabIndex = 9;
+            this.textBoxSpalte5.Visible = false;
+            // 
+            // textBoxSpalte4
+            // 
+            this.textBoxSpalte4.Location = new System.Drawing.Point(257, 212);
+            this.textBoxSpalte4.Name = "textBoxSpalte4";
+            this.textBoxSpalte4.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSpalte4.TabIndex = 10;
+            this.textBoxSpalte4.Visible = false;
+            // 
+            // textBoxSpalte3
+            // 
+            this.textBoxSpalte3.Location = new System.Drawing.Point(257, 184);
+            this.textBoxSpalte3.Name = "textBoxSpalte3";
+            this.textBoxSpalte3.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSpalte3.TabIndex = 11;
+            this.textBoxSpalte3.Visible = false;
+            // 
+            // textBoxSpalte2
+            // 
+            this.textBoxSpalte2.Location = new System.Drawing.Point(257, 157);
+            this.textBoxSpalte2.Name = "textBoxSpalte2";
+            this.textBoxSpalte2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSpalte2.TabIndex = 12;
+            this.textBoxSpalte2.Visible = false;
+            // 
+            // buttonTabelleErstellen
+            // 
+            this.buttonTabelleErstellen.Location = new System.Drawing.Point(257, 335);
+            this.buttonTabelleErstellen.Name = "buttonTabelleErstellen";
+            this.buttonTabelleErstellen.Size = new System.Drawing.Size(100, 87);
+            this.buttonTabelleErstellen.TabIndex = 13;
+            this.buttonTabelleErstellen.Text = "Tabelle erstellen";
+            this.buttonTabelleErstellen.UseVisualStyleBackColor = true;
+            this.buttonTabelleErstellen.Visible = false;
+            // 
+            // pictureBoxTabellenname
+            // 
+            this.pictureBoxTabellenname.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxTabellenname.Image")));
+            this.pictureBoxTabellenname.Location = new System.Drawing.Point(375, 96);
+            this.pictureBoxTabellenname.Name = "pictureBoxTabellenname";
+            this.pictureBoxTabellenname.Size = new System.Drawing.Size(24, 20);
+            this.pictureBoxTabellenname.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxTabellenname.TabIndex = 14;
+            this.pictureBoxTabellenname.TabStop = false;
+            this.pictureBoxTabellenname.Click += new System.EventHandler(this.pictureBoxTabellenname_Click);
+            // 
+            // pictureBoxSpalte1
+            // 
+            this.pictureBoxSpalte1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxSpalte1.Image")));
+            this.pictureBoxSpalte1.Location = new System.Drawing.Point(375, 131);
+            this.pictureBoxSpalte1.Name = "pictureBoxSpalte1";
+            this.pictureBoxSpalte1.Size = new System.Drawing.Size(24, 20);
+            this.pictureBoxSpalte1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxSpalte1.TabIndex = 15;
+            this.pictureBoxSpalte1.TabStop = false;
+            this.pictureBoxSpalte1.Visible = false;
+            // 
+            // pictureBoxSpalte2
+            // 
+            this.pictureBoxSpalte2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxSpalte2.Image")));
+            this.pictureBoxSpalte2.Location = new System.Drawing.Point(375, 157);
+            this.pictureBoxSpalte2.Name = "pictureBoxSpalte2";
+            this.pictureBoxSpalte2.Size = new System.Drawing.Size(24, 20);
+            this.pictureBoxSpalte2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxSpalte2.TabIndex = 16;
+            this.pictureBoxSpalte2.TabStop = false;
+            this.pictureBoxSpalte2.Visible = false;
+            // 
+            // pictureBoxSpalte3
+            // 
+            this.pictureBoxSpalte3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxSpalte3.Image")));
+            this.pictureBoxSpalte3.Location = new System.Drawing.Point(375, 184);
+            this.pictureBoxSpalte3.Name = "pictureBoxSpalte3";
+            this.pictureBoxSpalte3.Size = new System.Drawing.Size(24, 20);
+            this.pictureBoxSpalte3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxSpalte3.TabIndex = 17;
+            this.pictureBoxSpalte3.TabStop = false;
+            this.pictureBoxSpalte3.Visible = false;
+            // 
+            // pictureBoxSpalte5
+            // 
+            this.pictureBoxSpalte5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxSpalte5.Image")));
+            this.pictureBoxSpalte5.Location = new System.Drawing.Point(375, 242);
+            this.pictureBoxSpalte5.Name = "pictureBoxSpalte5";
+            this.pictureBoxSpalte5.Size = new System.Drawing.Size(24, 20);
+            this.pictureBoxSpalte5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxSpalte5.TabIndex = 18;
+            this.pictureBoxSpalte5.TabStop = false;
+            this.pictureBoxSpalte5.Visible = false;
+            // 
+            // pictureBoxSpalte4
+            // 
+            this.pictureBoxSpalte4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxSpalte4.Image")));
+            this.pictureBoxSpalte4.Location = new System.Drawing.Point(375, 212);
+            this.pictureBoxSpalte4.Name = "pictureBoxSpalte4";
+            this.pictureBoxSpalte4.Size = new System.Drawing.Size(24, 20);
+            this.pictureBoxSpalte4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxSpalte4.TabIndex = 19;
+            this.pictureBoxSpalte4.TabStop = false;
+            this.pictureBoxSpalte4.Visible = false;
             // 
             // FormNewTable
             // 
@@ -36,6 +257,26 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(896, 567);
             this.ControlBox = false;
+            this.Controls.Add(this.pictureBoxSpalte4);
+            this.Controls.Add(this.pictureBoxSpalte5);
+            this.Controls.Add(this.pictureBoxSpalte3);
+            this.Controls.Add(this.pictureBoxSpalte2);
+            this.Controls.Add(this.pictureBoxSpalte1);
+            this.Controls.Add(this.pictureBoxTabellenname);
+            this.Controls.Add(this.buttonTabelleErstellen);
+            this.Controls.Add(this.textBoxSpalte2);
+            this.Controls.Add(this.textBoxSpalte3);
+            this.Controls.Add(this.textBoxSpalte4);
+            this.Controls.Add(this.textBoxSpalte5);
+            this.Controls.Add(this.textBoxSpalte1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxTabellenname);
+            this.Controls.Add(this.pictureBoxBack);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -45,10 +286,39 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "FormNewTable";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTabellenname)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSpalte1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSpalte2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSpalte3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSpalte5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSpalte4)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxBack;
+        private System.Windows.Forms.TextBox textBoxTabellenname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxSpalte1;
+        private System.Windows.Forms.TextBox textBoxSpalte5;
+        private System.Windows.Forms.TextBox textBoxSpalte4;
+        private System.Windows.Forms.TextBox textBoxSpalte3;
+        private System.Windows.Forms.TextBox textBoxSpalte2;
+        private System.Windows.Forms.Button buttonTabelleErstellen;
+        private System.Windows.Forms.PictureBox pictureBoxTabellenname;
+        private System.Windows.Forms.PictureBox pictureBoxSpalte1;
+        private System.Windows.Forms.PictureBox pictureBoxSpalte2;
+        private System.Windows.Forms.PictureBox pictureBoxSpalte3;
+        private System.Windows.Forms.PictureBox pictureBoxSpalte5;
+        private System.Windows.Forms.PictureBox pictureBoxSpalte4;
     }
 }
